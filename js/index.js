9d7587462fea8f206b7802feee44ea33383f3238
averangeall <averangeall@gmail.com>
2014-06-29 01:43:41 +0800

    Pretty shadows behind captions

--- js/index.js
+++ js/index.js
@@ -1,16 +1,43 @@
 (function() {
     var memeBgName = 'am-i-the-only-one-around-here';
-    var upperCaptionContent = 'am i the only one around here';
-    var lowerCaptionContent = 'that comes from taiwan?';
+    var upperCaptionLines = ['am i the only one', 'around here'];
+    var lowerCaptionLines = ['that comes', 'from taiwan?'];
 
-    var meme = document.getElementById('meme');
-    var memeBg = document.getElementById('background-image');
-    var image = document.createElement('img');
-    image.setAttribute('src', 'images/' + memeBgName + '.jpg');
-    memeBg.appendChild(image);
+    var putMemeBg = function(memeBgName) {
+        var memeBg = document.getElementById('background-image');
+        var image = document.createElement('img');
+        image.setAttribute('src', 'images/' + memeBgName + '.jpg');
+        memeBg.appendChild(image);
+    };
 
-    var upperCaption = document.getElementById('upper-caption');
-    var lowerCaption = document.getElementById('lower-caption');
-    upperCaption.innerHTML = upperCaptionContent.toUpperCase();
-    lowerCaption.innerHTML = lowerCaptionContent.toUpperCase();
+    var putSingleCaption = function(captionLines, topBottom, color, horizontal, vertical) {
+        var captionGroup = document.getElementById('meme-caption-group');
+        var caption = document.createElement('div');
+        caption.className = 'meme-caption';
+        for(var i = 0; i < captionLines.length; ++ i) {
+            var line = document.createElement('div');
+            line.innerHTML = captionLines[i].toUpperCase();
+            caption.appendChild(line);
+        }
+        caption.style[topBottom] = (10 + vertical) + 'px';
+        caption.style.left = horizontal + 'px';
+        caption.style.color = color;
+        captionGroup.appendChild(caption);
+    };
+
+    var putPrettyCaption = function(captionLines, topBottom) {
+        var borderSize = 4;
+        for(var i = -borderSize; i <= borderSize; ++ i) {
+            for(var j = -borderSize; j <= borderSize; ++ j) {
+                putSingleCaption(captionLines, topBottom, 'black', i, j);
+            }
+        }
+        putSingleCaption(captionLines, topBottom, 'white', 0, 0);
+    };
+
+    putMemeBg(memeBgName);
+
+    putPrettyCaption(upperCaptionLines, 'top');
+    putPrettyCaption(lowerCaptionLines, 'bottom');
+
 })();
